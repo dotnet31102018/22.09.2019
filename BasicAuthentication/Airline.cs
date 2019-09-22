17b94cedd3a2daba27243d63145302d4299a5f4a
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-09-22 20:26:22 +0300

    Add files via upload

---
+++ BasicAuthentication/Airline.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _2209.Models
+{
+    public class Airline
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Password}";
+        }
+    }
+}
